e266eb770ab73b0b4011f619def9394156933b2a
Gbolahan <yustee2017@gmai.com>
2022-07-13 05:12:07 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Logo</h3>
+      <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>About</li>
+      </ul>
     </div>
   );
 }
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+function Test() {
+    return (
+        <div>
+            <h3>Logo</h3>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+} 
+
+
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"))
